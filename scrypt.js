1b898314648125f9c62fe537c7a47aa030995b8c
Mukhammad <23@.ru>
2021-12-25 04:51:53 -0600

    begin

---
+++ scrypt.js
@@ -0,0 +1,17 @@
+var myName = prompt('Enter your Name');
+console.log(myName);
+
+var age = prompt('Enter your age');
+console.log(age);
+
+var one = prompt('what will be ? 12 + 32 = ?' );
+var two = prompt('what will be ? 34 - 18 = ?');
+var three = prompt('what will be ? 6 * 9 = ?');
+var four = prompt('what will be ? 56 / 7 ?')
+alert('Open the Console to see the answers !');
+
+console.log('Your answers, check :');
+console.log('12 + 32 = 34 ' + '(your answer is ' + one + ')');
+console.log('34 - 18 = 16' + '(your answer is ' + two + ')');
+console.log('6 * 9 = 54' + '(your answer is ' + three + ')');
+console.log('56 / 7 = 8' + '(your answer is ' + four + ')');
